1995465297d603a093b76b3a3fab8e9a443255f0
Vinayak Pathak <vinayakzark@gmail.com>
2018-01-15 00:43:35 -0500

    Add implementation for ema

--- src/main/scala/com/vinpat/timestream/Timed.scala
+++ src/main/scala/com/vinpat/timestream/Timed.scala
@@ -1,12 +1,23 @@
 package com.vinpat.timestream
 
-import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.time.{Duration, LocalDateTime}
+
 import fs2._
 import cats._
 import cats.syntax.functor._
 import cats.effect._
 
-case class Timed[A](value: A, dateTime: LocalDateTime)
+case class Timed[A](value: A, dateTime: LocalDateTime) {
+  def <->(that: Timed[A]) = {
+    if (that.dateTime.isEqual(LocalDateTime.MIN)) {
+      Double.PositiveInfinity
+    }
+    else {
+      that.dateTime.until(this.dateTime, ChronoUnit.MILLIS).toDouble
+    }
+  }
+}
 
 object Timed {
   type TimedStream[F[_], A] = Stream[F, Timed[A]]
@@ -16,11 +27,18 @@ object Timed {
   }
 
   def now[A](a: A) = Timed(a, LocalDateTime.now)
+
+  def atMinTime[A](a: A) = Timed(a, LocalDateTime.MIN)
 }
 
 object Smoothing {
   import Timed._
-  def ema[F[_]]: Pipe[F, Timed[Double], Timed[Double]] = s => s.map(td => td.map(_ + 1)) //stub implementation
+  def ema[F[_]](lambda: Double): Pipe[F, Timed[Double], Timed[Double]] = s => s.scan[Timed[Double]](atMinTime(0)){(avg, td) =>
+    val timeDiff = td <-> avg
+    val lambdaPerMilli = lambda/1000
+    val newAvg = avg.value * math.exp(-lambdaPerMilli*timeDiff) + td.value * (1 - math.exp(-lambdaPerMilli*timeDiff))
+    td.copy(value = newAvg)
+  }
 }
 
 
@@ -32,7 +50,7 @@ object Main extends App {
   val in2: TimedStream[Pure, Double] = Stream(now(1.0), now(2.12), now(5.12), now(10.33))
 
   val out = in1
-    .through[Timed[Double]](ema)
+    .through[Timed[Double]](ema(0.1))
     .observe1(td => IO(println(td)))
-  out.compile.toVector.unsafeRunAsync(_ => ())
+  out.compile.toVector.unsafeRunAsync(e => println(e))
 }
