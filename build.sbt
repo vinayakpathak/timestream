05c7abb140f809fb072591aaa1c184b709d6e4eb
Vinayak Pathak <vinayakzark@gmail.com>
2018-01-14 21:43:16 -0500

    Basic FS2 structure

--- build.sbt
+++ build.sbt
@@ -2,4 +2,9 @@ name := "timestream"
 version := "0.0.1"
 scalaVersion := "2.11.8"
 
+//fork in run := true
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.1"
 libraryDependencies += "co.fs2" %% "fs2-core" % "0.9.7"
+libraryDependencies += "co.fs2" %% "fs2-io" % "0.9.7"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "0.5"
--- src/main/scala/com/vinpat/timestream/Timed.scala
+++ src/main/scala/com/vinpat/timestream/Timed.scala
@@ -1,9 +1,36 @@
 package com.vinpat.timestream
 
 import java.time.LocalDateTime
+import fs2._
+import cats._
+import cats.syntax.functor._
 
 case class Timed[A](value: A, dateTime: LocalDateTime)
 
 object Timed {
   type TimedStream[F[_], A] = Stream[F, Timed[A]]
+
+  implicit val timedFunctor: Functor[Timed] = new Functor[Timed] {
+    def map[A, B](ta: Timed[A])(f: A => B): Timed[B] = ta.copy(value = f(ta.value))
+  }
+
+  def now[A](a: A) = Timed(a, LocalDateTime.now)
+}
+
+object Smoothing {
+  import Timed._
+  def ema[F[_]]: Pipe[F, Timed[Double], Timed[Double]] = s => s.map(td => td.map(_ + 1)) //stub implementation
+}
+
+object Main extends App {
+  import Timed._
+  import Smoothing._
+
+  val in1: TimedStream[Task, Double] = Stream.repeatEval(Task.delay(scala.io.StdIn.readLine().toDouble)).map(now)
+  val in2: TimedStream[Nothing, Double] = Stream(now(1.0), now(2.12), now(5.12), now(10.33))
+
+  val out = in1
+    .through[Timed[Double]](ema)
+    .evalMap[Task, Task, Unit](td => Task.now(println(td)))
+  out.runLog.unsafeRun()
 }
