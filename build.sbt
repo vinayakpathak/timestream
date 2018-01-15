14573a0ddcd6bc10d815b66b590d7a16c939712d
Vinayak Pathak <vinayakzark@gmail.com>
2018-01-14 23:31:31 -0500

    Upgrade to fs2 0.10.0

--- build.sbt
+++ build.sbt
@@ -5,6 +5,8 @@ scalaVersion := "2.11.8"
 //fork in run := true
 
 libraryDependencies += "org.typelevel" %% "cats-core" % "1.0.1"
-libraryDependencies += "co.fs2" %% "fs2-core" % "0.9.7"
-libraryDependencies += "co.fs2" %% "fs2-io" % "0.9.7"
-libraryDependencies += "org.typelevel" %% "cats-effect" % "0.5"
+// available for Scala 2.11, 2.12
+libraryDependencies += "co.fs2" %% "fs2-core" % "0.10.0-RC1" // For cats 1.0.1 and cats-effect 0.8
+
+// optional I/O library
+libraryDependencies += "co.fs2" %% "fs2-io" % "0.10.0-RC1"
--- src/main/scala/com/vinpat/timestream/Timed.scala
+++ src/main/scala/com/vinpat/timestream/Timed.scala
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 import fs2._
 import cats._
 import cats.syntax.functor._
+import cats.effect._
 
 case class Timed[A](value: A, dateTime: LocalDateTime)
 
@@ -22,15 +23,16 @@ object Smoothing {
   def ema[F[_]]: Pipe[F, Timed[Double], Timed[Double]] = s => s.map(td => td.map(_ + 1)) //stub implementation
 }
 
+
 object Main extends App {
   import Timed._
   import Smoothing._
 
-  val in1: TimedStream[Task, Double] = Stream.repeatEval(Task.delay(scala.io.StdIn.readLine().toDouble)).map(now)
-  val in2: TimedStream[Nothing, Double] = Stream(now(1.0), now(2.12), now(5.12), now(10.33))
+  val in1: TimedStream[IO, Double] = Stream.repeatEval(IO(scala.io.StdIn.readLine().toDouble)).map(now)
+  val in2: TimedStream[Pure, Double] = Stream(now(1.0), now(2.12), now(5.12), now(10.33))
 
   val out = in1
     .through[Timed[Double]](ema)
-    .evalMap[Task, Task, Unit](td => Task.now(println(td)))
-  out.runLog.unsafeRun()
+    .observe1(td => IO(println(td)))
+  out.compile.toVector.unsafeRunAsync(_ => ())
 }
